c7749b6a9adc7cfd2549ebe55a4284925a67ba87
Mehdi Basli <mehdybasly@gmail.com>
2022-11-05 23:00:18 +0100

    interceptor + authService added user can signin and signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,34 @@
-import { Controller, Post, Body, Get, Param, Query, Delete, Patch, NotFoundException } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query, Delete, Patch, NotFoundException, SerializeOptions, UseInterceptors } from '@nestjs/common';
 import { NotFoundError } from 'rxjs';
+import { serialized, SerializedInterceptor } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { userDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@serialized(userDto)
 export class UsersController {
-    constructor(private usersService: UsersService){}
+    constructor(private usersService: UsersService , 
+        private authSerive : AuthService){}
+
+
+
     @Post('/signup')
     create(@Body() body : CreateUserDto){
-    return this.usersService.create(body.email , body.password);
-}
+    return this.authSerive.signUp(body.email , body.password);
+    }
+
+    @Post('/signin')
+    signin(@Body() body: CreateUserDto){
+        return this.authSerive.signIn(body.email , body.password)
+    }
+
 
 @Get('/:id')
 async findUser(@Param('id') id:string){
+   
 const user = await this.usersService.findone(parseInt(id));
 if (!user){
     throw new NotFoundException('user not found')
